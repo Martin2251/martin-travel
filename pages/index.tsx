fef70ae5dab523d6aaedc8f55bb6ef627f1cbf87
Martin Lowe <martinlowe673@gmail.com>
2024-05-14 08:00:00 +0100

    start of navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import Navbar from '@/Components/Navbar'
 import React from 'react'
 
 const index = () => {
   return (
-    <div className='text-9xl text-red-700'>
-      hoem page 
+    <div className='overflow-x-hidden'>
+        <Navbar  />
     </div>
   )
 }
